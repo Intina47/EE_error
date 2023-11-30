c888013395f70fe509ecf9bb2b1f45dd57bc756d
uzohou <isaiahntina47@gmail.com>
2023-11-30 01:34:06 +0000

    Update main.cpp: Modify crawl depth and add TODOs for parallel crawling and data cleanup

--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@
 #include <gumbo.h>
 #include "crawler.h"
 
-int main() {
+int main(int argc, char const *argv[])
+{
     WebCrawler crawler;
-    std::string url = "https://en.wikipedia.org/wiki/Main_Page";
+    std::string url = "https://www.kenyans.co.ke/news/95077-remark-our-papers-kcpe-candidate-demands-after-knec-admits-errors";
     std::vector<std::string> keywords = {"#NoExcuse", "VAWG", "Spotlight", "war",};
-    crawler.crawlDepth(url, 2, keywords);
-
+    int depth = 2;
+    if (argc > 1) {
+        depth = std::stoi(argv[1]);
+    }
+    crawler.crawlDepth(url, depth, keywords);
     return 0;
 }
+
+// TODO: Implement parallel crawling using threads or processes (or both) to speed up crawling
+// TODO: Clean up data returned from crawler 
