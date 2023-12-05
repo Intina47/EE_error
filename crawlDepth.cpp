f43b6e89027370082c3c5763cc1ff3d20fcab72b
uzohou <isaiahntina47@gmail.com>
2023-12-05 17:46:25 +0000

    Add queue data structure for URL crawling

--- crawlDepth.cpp
+++ crawlDepth.cpp
@@ -6,6 +6,7 @@
 #include "crawler.h"
 #include <regex>
 #include <fstream>
+#include <queue>
 
 void WebCrawler::crawlDepth(const std::string& url, int depth, const std::vector<std::string>& keywords) {
     if (depth < 0) {
@@ -13,51 +14,59 @@ void WebCrawler::crawlDepth(const std::string& url, int depth, const std::vector
         return;
     }
 
-    std::string html = crawl(url);
-    std::string source = "Source: " + url;
-    std::string text = extractText(html, source);
+    // assign keywords to a variable
+    this->keywords = keywords;
 
-    // Search keywords in the current page
-    std::vector<std::string> keywordResults = searchHeadlines(text, source);
-    if(!keywordResults.empty()) {
-        std::cout << source << std::endl;
-        std::cout << keywordResults.size() << std::endl;
-        for (auto& result : keywordResults) {
-                std::cout << result << std::endl;
-        }
-    } else {
-            std::cerr << "No Search results found on: " << url << std::endl;
-        }
+    //queue of urls to crawl
+    std::queue<std::string> linkQueue;
+    //add the current url to the queue
+    linkQueue.push(url);
 
-    if (depth > 0) {
+    while(!linkQueue.empty()){
+        // pop the front url from the queue
+        std::string currentUrl = linkQueue.front();
+        linkQueue.pop();
+
+        std::string html = crawl(currentUrl);
+        std::string source = "Source: " + currentUrl;
+        std::string text = extractText(html, source);
+
+        // Search keywords in the current page
+        std::vector<std::string> keywordResults = searchHeadlines(text, source);
+        if(!keywordResults.empty()) {
+            std::cout << source << std::endl;
+            std::cout << keywordResults.size() << std::endl;
+            for (auto& result : keywordResults) {
+                    std::cout << result << std::endl;
+            }
+        } else {
+                std::cerr << "No Search results found on: " << currentUrl << std::endl;
+            }
+
+        if (depth > 0) {
         // Extract links and crawl each linked page with reduced depth
         std::vector<std::string> links = extractLinks(html);
         if(!links.empty()) {
-            std::cout << "Links at: " << url << std::endl;
+            std::cout << "Links at: " << currentUrl << std::endl;
             std::cout << links.size() << std::endl;
-            std::vector<std::string> validLinks;
             for (const auto& link : links) {
                 if (isValidUrl(link)) {
                     // Check if the link has already been visited
                     if (visitedUrls.find(link) == visitedUrls.end()) {
-                        validLinks.push_back(link);
+                        linkQueue.push(link);
                         visitedUrls.insert(link);
                         // write to file
                         std::ofstream myfile;
                         myfile.open ("links.txt", std::ios_base::app);
                         myfile << link << std::endl;
                         myfile.close();
-
                     } else {
                         std::cerr << "Link already visited: " << link << std::endl;
                     }
                 }
             }
-            for (const auto& link : validLinks) {
-                crawlDepth(link, depth - 1, keywords);
-            }
         } else {
-            std::cerr << "No links found on: " << url << std::endl;
+            std::cerr << "No links found on: " << currentUrl << std::endl;
         }
 
         // Check for pagination and crawl next pages
@@ -68,14 +77,15 @@ void WebCrawler::crawlDepth(const std::string& url, int depth, const std::vector
                     // Check if the link has already been visited
                     if (visitedUrls.find(paginationLink) == visitedUrls.end()) {
                         visitedUrls.insert(paginationLink);
-                        crawlDepth(paginationLink, depth, keywords);
+                        linkQueue.push(paginationLink);
                     } else {
                         std::cerr << "Link already visited: " << paginationLink << std::endl;
                     }
                 }
             }
         } else {
-            std::cerr << "No pagination links found on: " << url << std::endl;
+            std::cerr << "No pagination links found on: " << currentUrl << std::endl;
         }
+    }
     }
 }
