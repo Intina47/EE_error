3488745a591b1a350632b1762259f10425948fdb
uzohou <isaiahntina47@gmail.com>
2023-12-05 17:44:59 +0000

    Update URL in main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 int main(int argc, char const *argv[])
 {
     WebCrawler crawler;
-    std::string url = "https://www.kenyans.co.ke/news/95077-remark-our-papers-kcpe-candidate-demands-after-knec-admits-errors";
+    std::string url = "https://en.wikipedia.org/wiki/Elon_Musk";
     std::vector<std::string> keywords = {"#NoExcuse", "VAWG", "Spotlight", "war",};
     int depth = 2;
     if (argc > 1) {
@@ -17,6 +17,3 @@ int main(int argc, char const *argv[])
     crawler.crawlDepth(url, depth, keywords);
     return 0;
 }
-
-// TODO: Implement parallel crawling using threads or processes (or both) to speed up crawling
-// TODO: Clean up data returned from crawler 
