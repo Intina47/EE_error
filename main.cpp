a01806c15f82e4ab898dfb88ff1ca85ff5e0eeb5
uzohou <isaiahntina47@gmail.com>
2023-12-06 06:14:20 +0000

    Add parallelCrawler class and update main function

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <curl/curl.h>
 #include <gumbo.h>
 #include "crawler.h"
+#include "parallelCrawler.h"
 
-int main(int argc, char const *argv[])
+int main( int argc, char** argv )
 {
     WebCrawler crawler;
     std::string url = "https://en.wikipedia.org/wiki/Elon_Musk";
@@ -16,4 +17,6 @@ int main(int argc, char const *argv[])
     }
     crawler.crawlDepth(url, depth, keywords);
     return 0;
+    // run the program with the following command:
+    // ./main 2
 }
